f669b1270d707c6a52fe3981780f7dbf548ce267
Sarim <sarimk80@gmail.com>
2020-04-15 13:53:18 +0500

    friendCheck list implemented

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,13 +4,75 @@ import admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase)
 
 export const onMessageReceived = functions.firestore.document('Messages/{uniqueId}/PrivateMessage/{messageId}').onCreate(
-    (snapshot, context) => {
+    async (snapshot, context) => {
 
         console.log('${uniqueId} ${messageId}')
         console.log(snapshot.data())
         const messageDetail = snapshot.data()!
         console.log(messageDetail.date)
-        //admin.firestore().collection("").doc().collection("").add({ "": "" })
+
+        let sender = await admin.firestore().collection("Users").doc(messageDetail.fromUuid).get()
+        let receiver = await admin.firestore().collection("Users").doc(messageDetail.toUuid).get()
+
+        let senderData = sender.data()!
+        let receiverData = receiver.data()!
+
+        console.log(receiverData.uuid)
+        console.log(senderData.name)
+
+        const senderCheck = {
+            "date": Date.now.toString(),
+            "friendUser": {
+                "uuid": receiverData.uuid,
+                "name": receiverData.name,
+                "email": receiverData.email,
+                "phoneNumber": receiverData.phoneNumber,
+                "imageUrl": receiverData.imageUrl,
+                "latitude": receiverData.latitude,
+                "longitude": receiverData.longitude
+            },
+            "userId": {
+                "uuid": senderData.uuid,
+                "name": senderData.name,
+                "email": senderData.email,
+                "phoneNumber": senderData.phoneNumber,
+                "imageUrl": senderData.imageUrl,
+                "latitude": senderData.latitude,
+                "longitude": senderData.longitude
+            }
+        }
+
+        const receiverCheck = {
+            "date": Date.now.toString(),
+            "friendUser": {
+                "uuid": senderData.uuid,
+                "name": senderData.name,
+                "email": senderData.email,
+                "phoneNumber": senderData.phoneNumber,
+                "imageUrl": senderData.imageUrl,
+                "latitude": senderData.latitude,
+                "longitude": senderData.longitude
+            },
+            "userId": {
+                "uuid": receiverData.uuid,
+                "name": receiverData.name,
+                "email": receiverData.email,
+                "phoneNumber": receiverData.phoneNumber,
+                "imageUrl": receiverData.imageUrl,
+                "latitude": receiverData.latitude,
+                "longitude": receiverData.longitude
+            }
+
+        }
+
+        await admin.firestore().collection("Users").doc(messageDetail.toUuid).collection("FriendsCheck").doc(messageDetail.fromUuid).set(receiverCheck)
+
+
+        await admin.firestore().collection("Users").doc(messageDetail.fromUuid).collection("FriendsCheck").doc(messageDetail.toUuid).set(senderCheck)
+
+
+        return await admin.firestore().collection("Users").doc(messageDetail.toUuid).collection("MessageRead").doc("isRead").set({ "isRead": false })
+
     })
 
 
